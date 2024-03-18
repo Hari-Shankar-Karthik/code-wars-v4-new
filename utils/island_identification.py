9875512633d5b036e79288acc17a2d06b0089463
Ratan <ratanskokal@gmail.com>
2024-03-18 17:09:02 +0530

    Fixed bugs

--- utils/island_identification.py
+++ utils/island_identification.py
@@ -1,4 +1,4 @@
-def ActPirate(pirate):
+def updateIslandCord(pirate):
     up = pirate.investigate_up()[0]
     ne = pirate.investigate_ne()[0]
     nw = pirate.investigate_nw()[0]
@@ -9,11 +9,7 @@ def ActPirate(pirate):
     left = pirate.investigate_left()[0]
     x, y = pirate.getPosition()
 
-    if (
-        (up == "island1" and s[0] != "myCaptured")
-        or (up == "island2" and s[1] != "myCaptured")
-        or (up == "island3" and s[2] != "myCaptured")
-    ):
+    if (up[:-1] == "island"):
         if (up == ne and up == nw):
             s = up[-1] + str(x) + "," + str(y - 2)
         elif (up == ne):
@@ -24,26 +20,18 @@ def ActPirate(pirate):
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
 
-    elif (
-        (down == "island1" and s[0] != "myCaptured")
-        or (down == "island2" and s[1] != "myCaptured")
-        or (down == "island3" and s[2] != "myCaptured")
-    ):
+    elif (down[:-1] == "island"):
         if (down == se and down == sw):
             s = down[-1] + str(x) + "," + str(y + 2)
         elif (down == se):
-            s = down[-1] + str(x-1) + "," + str(y + 2)
-        else:
             s = down[-1] + str(x+1) + "," + str(y + 2)
+        else:
+            s = down[-1] + str(x-1) + "," + str(y + 2)
 
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
 
-    elif (
-        (left == "island1" and s[0] != "myCaptured")
-        or (left == "island2" and s[1] != "myCaptured")
-        or (left == "island3" and s[2] != "myCaptured")
-    ):
+    elif (left[:-1] == "island"):
         if (left == nw and left == sw):
             s = left[-1] + str(x - 2) + "," + str(y)
         elif (left == nw):
@@ -54,11 +42,7 @@ def ActPirate(pirate):
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
 
-    elif (
-        (right == "island1" and s[0] != "myCaptured")
-        or (right == "island2" and s[1] != "myCaptured")
-        or (right == "island3" and s[2] != "myCaptured")
-    ):
+    elif (right[:-1] == "island"):
         if(right == ne and right == se):
             s = right[-1] + str(x + 2) + "," + str(y)
         elif(right == ne):
@@ -69,41 +53,22 @@ def ActPirate(pirate):
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
     
-    elif (
-        (ne == "island1" and s[0] != "myCaptured")
-        or (ne == "island2" and s[1] != "myCaptured")
-        or (ne == "island3" and s[2] != "myCaptured")
-    ):
+    elif (ne[:-1] == "island"):
         s = ne[-1] + str(x + 2) + "," + str(y-2)
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
 
-    elif (
-        (se == "island1" and s[0] != "myCaptured")
-        or (se == "island2" and s[1] != "myCaptured")
-        or (se == "island3" and s[2] != "myCaptured")
-    ):
+    elif (se[:-1] == "island"):
         s = se[-1] + str(x + 2) + "," + str(y+2)
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
 
-    elif (
-        (nw == "island1" and s[0] != "myCaptured")
-        or (nw == "island2" and s[1] != "myCaptured")
-        or (nw == "island3" and s[2] != "myCaptured")
-    ):
+    elif (nw[:-1] == "island"):
         s = nw[-1] + str(x - 2) + "," + str(y-2)
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
 
-    elif (
-        (sw == "island1" and s[0] != "myCaptured")
-        or (sw == "island2" and s[1] != "myCaptured")
-        or (sw == "island3" and s[2] != "myCaptured")
-    ):
+    elif (sw[:-1] == "island"):
         s = sw[-1] + str(x - 2) + "," + str(y + 2)
         if(island_cord[int(s[0])] == ""):
             island_cord[int(s[0])] = s[1:]
-
-def ActTeam(team):
-    pass

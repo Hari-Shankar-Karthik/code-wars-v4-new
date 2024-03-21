b5dc632faae7c12814513194971222cd93c85927
Hari Shankar Karthik <harishankarkarthik.93@gmail.com>
2024-03-21 11:55:13 +0530

    MOVED: all .md files to notes directory

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from engine.main import Game
 import scriptred, scriptblue, hari_script
 
 if __name__ == "__main__":
-    G = Game((40, 40), hari_script, hari_script)
+    G = Game((40, 40), hari_script, scriptblue)
     G.run_game()
